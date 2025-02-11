f3b97c50361adf004e69ffc27d424433a3687084
Aman Singh <00amansinghh00@gmail.com>
2025-02-11 20:11:16 +0530

    Update Detail.js

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -4,6 +4,7 @@ import { Typography, Stack, Button } from '@mui/material';
 import BodyPartImage from '../assets/icons/body-part.png';
 import TargetImage from '../assets/icons/target.png';
 import EquipmentImage from '../assets/icons/equipment.png';
+import Filter from './Filter';
 
 const Detail = ({ exerciseDetail }) => {
   const { bodyPart, gifUrl, name, target, equipment } = exerciseDetail;
@@ -46,6 +47,7 @@ const Detail = ({ exerciseDetail }) => {
             </Typography>
           </Stack>
         ))}
+           <Filter/>
       </Stack>
     </Stack>
   );

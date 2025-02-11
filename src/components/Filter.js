6e20aadde35c9c2686c3352abfdd8204827d27f1
Aman Singh <00amansinghh00@gmail.com>
2025-02-11 20:23:28 +0530

    Update Filter.js

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,9 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Button, Popover, Typography, Slider } from "@mui/material";
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
+
 
 function Filter() {
   const [anchorEl, setAnchorEl] = useState(null);
